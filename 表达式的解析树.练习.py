70d87c21c29481bf437d2596c428fd3fba601c02
Gabriel_Zhong <MingPeano@gmail.com>
2024-08-20 04:22:53 +0800

    解析树代码更新

--- 表达式的解析树.练习.py
+++ 表达式的解析树.练习.py
@@ -1,6 +1,5 @@
 class BuildParseTree:
     def __init__(self, value=None):
-        self.node = None
         self.value = value
         self.left = None
         self.right = None
@@ -20,6 +19,7 @@ class BuildParseTree:
             t = BuildParseTree(value)
             t.right = self.right
             self.right = t
+
 
 def build_tree(expression):
     t = BuildParseTree()  # 初始化一个树
@@ -51,6 +51,7 @@ def build_tree(expression):
 #     else:
 #         return eval(''.join([evaluate(tree.left), tree.value, evaluate(tree.right)]))
 
+ # 利用前序遍历求解表达式的值
 def evaluate(tree):
     if tree.left is None and tree.right is None:
         return tree.value
@@ -58,8 +59,21 @@ def evaluate(tree):
     right_val = evaluate(tree.right)
     return eval(f'{left_val} {tree.value} {right_val}')
 
+def inorder_traversal(tree):
+    if tree is None:
+        return ""
+    left_expr = inorder_traversal(tree.left)
+    right_expr = inorder_traversal(tree.right)
+    if left_expr or right_expr:
+        return f"({left_expr}{tree.value}{right_expr})"
+    else:
+        return str(tree.value)
+
+
+
 if __name__ == '__main__':
-    expression = "(3+(4*5))"
+    expression = "(3+(5*(4+5)))" # 必须是全括号表达式
     parse_tree = build_tree(expression)
     result = evaluate(parse_tree)
-    print(result)  # 输出 23
+    print(result)
+    print(inorder_traversal(parse_tree))
